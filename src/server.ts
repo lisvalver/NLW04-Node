f7439ac7ec82ccba1efd77ae11910c0f40dde66d
lisvalver <lisvalver.atte@gmail.com>
2021-02-27 20:47:37 -0300

    NLW04 Node - Dia 02 - Criando model, controller e rota de usuario

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import 'reflect-metadata'
 import express, { response } from 'express';
 import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Welcome to Next Level!  NLW 04 - Node"});
-});
-
-app.post("/", (request, response) => {
-  return response.json({ message: "Data saved successfully!"});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
