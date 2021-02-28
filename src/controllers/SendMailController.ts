593f3b1b7ca32c9deb5f324432ea2f155be7d61d
lisvalver <lisvalver.atte@gmail.com>
2021-02-28 16:20:30 -0300

    NLW04 Node - Dia 04 - Criando serviço de email usando Nodemailer e Ethereal

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
+import SendMailService from "../services/SendMailService";
 
 class SendMailController {
   async execute(request: Request, response: Response) {
@@ -20,11 +21,11 @@ class SendMailController {
       });
     }
 
-    const surveyAlreadyExists = await surveysRepository.findOne({
+    const survey = await surveysRepository.findOne({
       id: survey_id,
     });
 
-    if (!surveyAlreadyExists) {
+    if (!survey) {
       return response.status(400).json({
         error: "Survey does not exists!"
       });
@@ -35,6 +36,8 @@ class SendMailController {
       survey_id,
     });
     await surveysUsersRepository.save(surveyUser);
+
+    await SendMailService.execute(email, survey.title, survey.description);
 
     return response.json(surveyUser);
   }
